fafa8e5bb801bd2e966ff5533b709356865c8deb
c--w <zevrnc@gmail.com>
2023-07-05 13:53:35 +0200

    or

--- main.js
+++ main.js
@@ -1,42 +1,50 @@
-const N=100000;
+const N = 100000;
 function calculate() {
     let dices = $('#dices').val();
     let tries = $('#tries').val();
     let target = $('#target').val();
-    target = target.split("").sort();
     let okeys = 0;
     for (let i = 0; i < N; i++) {
-        for(let t=0; t<tries; t++) {
+        for (let t = 0; t < tries; t++) {
             let diceArr = randomDice(dices);
-            if(compare(target, randomDice(dices))) {
+            if (compare(target, randomDice(dices))) {
                 okeys++;
                 break;
             }
         }
     }
-    $('#result').html(Math.round(okeys/N*100) + "%");
+    $('#result').html(Math.round(okeys / N * 100) + "%");
 }
 
 function compare(a, b) {
     let ok = true;
-    for(let i=0; i<a.length; i++) {
-        if(a[i] == '?')
-            continue;
-        let num = Number(a[i]);
-        if(!b.includes(num)) {
-            ok = false;
-            break;
-        } else {
-            b.splice(b.indexOf(num), 1);
+    let aa = a.split('|');
+    for (let k = 0; k < aa.length; k++) {
+        let target = aa[k].split('').sort();
+        let bb = [...b];
+        for (let i = 0; i < target.length; i++) {
+            if (target[i] == '?')
+                continue;
+            let num = Number(target[i]);
+            if (!bb.includes(num)) {
+                ok = false;
+                break;
+            } else {
+                bb.splice(bb.indexOf(num), 1);
+            }
         }
+        if(ok)
+            break;
+        else if(k < aa.length-1)
+            ok = true;
     }
     return ok;
 }
 
 function randomDice(dices) {
     let diceArr = [];
-    for(let d = 0; d< dices; d++) {
-        diceArr.push(Math.floor(Math.random()*6)+1);
+    for (let d = 0; d < dices; d++) {
+        diceArr.push(Math.floor(Math.random() * 6) + 1);
     }
     diceArr.sort();
     return diceArr;
